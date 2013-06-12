030a7dbed7ec585d85d7300df27e62c16b269a11
ksammand <kamalys7@gmail.com>
2013-06-12 21:11:34 +0300

    Update nombre_vote.sh
    1) Ajout de la possibilité de laisser l'utilisateur choisir une catégorie
    2) Le classement des valeurs dans l'ordre décroissant s'affiche en console

--- groupevdm/nombre_vote.sh
+++ groupevdm/nombre_vote.sh
@@ -1,19 +1,24 @@
-#Ce code permet d'afficher le nombre de "Je valide, c'est une VDM" de la catégorie "argent" Pour cela, il faut d'abord se placer dans le répertoire de la catégorie "argent" ou "amour" au choix 
+#Ce code permet d'afficher le nombre de "Je valide, c'est une VDM" de la catégorie "argent" Pour cela, il faut d'abord avoir téléchargé le site vie de merde dans son dossier personnel
+#L'utilisateur peut à présent choisir la catégorie dont il souhaite avoir le classement de 'Je valide c'est une VDM'
+#Les valeurs extraites sont stockées dans un fichier texte et on affiche en console un classement (valeurs affichées dans l'ordre décroissant)
 
 #!/bin/sh
-while true; do
-read -p "Argent ou amour ? " var # demande à l'utilisateur de choisir 
-  case $var in # test les entrées
-	argent) cd argent
-	echo "voic les résultats de la catégorie argent" 
-	break;; #arrete la boucle 
-	amour) cd amour 
-	break; # arrete la boucle
-	echo "voic les résultats de la catégorie amour";;
-*) echo "mauvaise entrée";; esac 
-done;
+echo "Entrez le nom d'une catégorie: " #On affiche à l'écran le texte indiqué qui invite l'utilisateur à entrer le nom d'une catégorie
+read word #On récupère la catégorie entrée par l'utilisateur
+
+cd www.viedemerde.fr/$word # On se place dans le répertoire www.viedemerde.fr et ensuite dans le répertoire correspondant à la catégorie choisie
+echo 'Voici les résultats de la catégorie '$word ;
 
 dossier=`echo ${PWD##*/}`; #je garde le nom du dossier dans une variable
+if [ -f ../classement.txt ] #On vérifie si le fichier classement.txt existe déjà
+then
+# si les fichiers existent:
+rm ../classement.txt  #On efface le fichier classement.txt existant
+
+touch ../classement.txt; #On recrée les fichiers classement.txt comme ça on a un fichier texte vide
+else  
+   touch ../classement.txt; #On crée les fichiers classement.txt
+fi
 for i in `ls` #je parcours tous les fichiers dans le dossier
 do
 
@@ -22,9 +27,11 @@ do
 tmp=${line##?*est une VDM</a> (<span class=}; # j'extrais le nombre de "Je valide, c'est une VDM" en indiquant ce qui est écrit avant dans le code source
 url=${tmp%%\</span>?*}; #J'indique ce qui se trouve dans le code source après l'information que je souhaite récupérer
 tmp2=${url##?*>};
-echo $tmp2 
+
+
+
+echo $tmp2 >> ../classement.txt; #On imprime les valeurs dans le fichier classement.txt
 
 done
 done
- 
-# Kamaly SAMMANDAMOURTHY et caroline ROMAIN  
+sort -r ../classement.txt; #On trie par ordre décroissant les éléments du fichier classement.txt et on affiche dans la console la liste des valeurs dans l'ordre décroissant, ainsi nous avons un classement
